e43bca88a8c8f3d33bd3b7485d050dab159ee217
Roman Gatek <roman.gatek@gmail.com>
2024-12-15 13:00:53 +0100

    - create employeeControler - method to get list of all employees - method to get employee by ID

---
+++ src/main/java/org/example/employees/controllers/EmployeeController.java
@@ -0,0 +1,36 @@
+package org.example.employees.controllers;
+
+import org.example.employees.models.Employee;
+import org.example.employees.services.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.addEmployee(employee));
+    }
+}

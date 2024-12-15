dde1e3f5cd36e9856ac4ea2eb251dcf0998d4f15
Stefan <stefan.pocsik@gmail.com>
2024-12-15 10:29:43 +0100

    initial commit

---
+++ src/main/java/org/example/employees/EmployeesApplication.java
@@ -0,0 +1,13 @@
+package org.example.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/employees/configs/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package org.example.employees.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/", true)
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                );
+
+        return http.build();
+    }
+}

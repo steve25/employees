a57cb6f434657184f6de788fea5324132bd86d3d
Stefan <stefan.pocsik@gmail.com>
2024-12-15 11:08:53 +0100

    fix: getter and setter

--- src/main/java/org/example/employees/models/Attendance.java
+++ src/main/java/org/example/employees/models/Attendance.java
@@ -1,11 +1,14 @@
 package org.example.employees.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "attendances")
+@Getter @Setter
 public class Attendance extends BaseEntity {
     @Column(nullable = false)
     private LocalDate date;

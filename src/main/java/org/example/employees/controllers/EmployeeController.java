7ddf174d167ae3cd8661eee50d0da1e50e400799
Roman Gatek <roman.gatek@gmail.com>
2024-12-15 13:38:47 +0100

    - create employeeControler, employeeService - method to get list of all employees - method to get employee by ID

--- src/main/java/org/example/employees/controllers/EmployeeController.java
+++ src/main/java/org/example/employees/controllers/EmployeeController.java
@@ -2,8 +2,6 @@ package org.example.employees.controllers;
 
 import org.example.employees.models.Employee;
 import org.example.employees.services.EmployeeService;
-import org.springframework.boot.Banner;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/employees")
+@RequestMapping("/")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -30,7 +28,7 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public String getEmployeeById(@PathVariable Long id, Model model) {
         Employee employee = employeeService.getEmployeeById(id).orElse(null);
-        if (employee != null) {
+        if (employee == null) {
             return "employee/not-found";
         }
         model.addAttribute("employee", employee);
---
+++ src/main/java/org/example/employees/services/EmployeeService.java
@@ -0,0 +1,26 @@
+package org.example.employees.services;
+
+import org.example.employees.models.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+}
